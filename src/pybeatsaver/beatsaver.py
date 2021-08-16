bee59cd60776db8682436853b82cd81209a1510d
LuCkEr <lucker@lucker.xyz>
2021-08-16 03:21:19 +0300

    Now using async

--- src/pybeatsaver/beatsaver.py
+++ src/pybeatsaver/beatsaver.py
@@ -3,7 +3,7 @@ from json.decoder import JSONDecodeError
 from typing import Dict
 
 import requests
-from outcache import Cache
+from outcache import CacheAsync
 
 from .common import Common
 from .models.map_detail import MapDetail
@@ -16,31 +16,31 @@ class BeatSaver:
     def __init__(self):
         self.log = logging.getLogger(__name__)
 
-    def _process_url(self, url: str) -> Dict:
-        response = Common.request(requests.get, url, timeout=self.TIMEOUT)
+    async def _process_url(self, url: str) -> Dict:
+        response = await Common.request(requests.get, url, timeout=self.TIMEOUT)
 
         try:
             data = response.json()
         except JSONDecodeError:
             self.log.exception("JSONDecodeError, response: %r, response.text: %r", response, response.text)
-            data = {"error": "Failed to decode json from BeatSaver. Somethings broken."}
+            data = {"error": "Failed to decode json from scoresaber. Somethings broken."}
 
         return data
 
-    @Cache(hours=24)
-    def _get_map_by_hash(self, song_hash: str):
-        return self._process_url(f"{self._url}/maps/hash/{song_hash}")
+    @CacheAsync(hours=24)
+    async def _get_map_by_hash(self, song_hash: str):
+        return await self._process_url(f"{self._url}/maps/hash/{song_hash}")
 
-    def get_map_by_hash(self, song_hash: str) -> MapDetail:
-        map_info = self._get_map_by_hash(song_hash)
+    async def get_map_by_hash(self, song_hash: str) -> MapDetail:
+        map_info = await self._get_map_by_hash(song_hash)
 
         return MapDetail.from_dict(map_info)
 
-    @Cache(hours=24)
-    def _get_map_by_key(self, song_key: str):
-        return self._process_url(f"{self._url}/maps/beatsaver/{song_key}")
+    @CacheAsync(hours=24)
+    async def _get_map_by_key(self, song_key: str):
+        return await self._process_url(f"{self._url}/maps/beatsaver/{song_key}")
 
-    def get_map_by_key(self, song_key: str) -> MapDetail:
-        map_info = self._get_map_by_key(song_key)
+    async def get_map_by_key(self, song_key: str) -> MapDetail:
+        map_info = await self._get_map_by_key(song_key)
 
         return MapDetail.from_dict(map_info)
--- tests/test_beatsaver.py
+++ tests/test_beatsaver.py
@@ -1,10 +1,10 @@
-import unittest
+from unittest import IsolatedAsyncioTestCase
 
 from src.pybeatsaver import NotFoundException
 from src.pybeatsaver.beatsaver import BeatSaver
 
 
-class TestBeatSaver(unittest.TestCase):
+class TestBeatSaver(IsolatedAsyncioTestCase):
     _valid_map_hash = "4bbdaa1004a00eeb9c8d9432640e1c7d490b46d9"
     _invalid_map_hash = "4BDAA104A00E32EB9CD941132640E1C7490B4932"
     _valid_map_key = "19487"
@@ -13,17 +13,19 @@ class TestBeatSaver(unittest.TestCase):
     def setUp(self):
         self.beatsaver = BeatSaver()
 
-    def test_get_map_by_hash_valid(self):
-        map_detail = self.beatsaver.get_map_by_hash(self._valid_map_hash)
+    async def test_get_map_by_hash_valid(self):
+        map_detail = await self.beatsaver.get_map_by_hash(self._valid_map_hash)
         self.assertEqual(map_detail.versions[0].hash, self._valid_map_hash)
 
-    def test_get_map_by_hash_invalid(self):
-        self.assertRaises(NotFoundException, self.beatsaver.get_map_by_hash, self._invalid_map_hash)
+    async def test_get_map_by_hash_invalid(self):
+        with self.assertRaises(NotFoundException):
+            await self.beatsaver.get_map_by_hash(self._invalid_map_hash)
 
-    def test_get_map_by_key(self):
-        map_detail = self.beatsaver.get_map_by_key(self._valid_map_key)
+    async def test_get_map_by_key(self):
+        map_detail = await self.beatsaver.get_map_by_key(self._valid_map_key)
 
         self.assertEqual(map_detail.versions[0].key, self._valid_map_key)
 
-    def test_get_map_by_key_invalid(self):
-        self.assertRaises(NotFoundException, self.beatsaver.get_map_by_key, self._invalid_map_key)
+    async def test_get_map_by_key_invalid(self):
+        with self.assertRaises(NotFoundException):
+            await self.beatsaver.get_map_by_key(self._invalid_map_key)
