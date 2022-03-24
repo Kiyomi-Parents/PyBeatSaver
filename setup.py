7997eb10652b57ca942edfea10dd15ecba356cac
LuCkEr <lucker@lucker.xyz>
2022-03-24 22:21:21 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("requirements.txt") as f:
 
 setuptools.setup(
     name="PyBeatSaver",
-    version="0.1.5",
+    version="0.1.6",
     author="LuCkEr-",
     author_email="lucker@lucker.xyz",
     description="Beat Saver API client",
