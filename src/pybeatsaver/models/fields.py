b42d5dc21f577fbc663eab669228052324cec5d8
lihailin <hailin.li.io@outlook.com>
2024-12-16 23:46:03 +0800

    Omit fields with None value in JSON serialization

--- src/pybeatsaver/models/fields.py
+++ src/pybeatsaver/models/fields.py
@@ -6,6 +6,9 @@ from typing import *
 from dataclasses_json import config
 from marshmallow import fields
 
+def value_is_none(value: Any) -> bool:
+    return value is None
+
 def datetime_encoder_iso(dt: Optional[datetime]) -> Optional[str]:
     return dt.isoformat() if dt is not None else None
 
@@ -17,7 +20,8 @@ def datetime_field(json_field_name: Optional[str] = None) -> Field:
         conf = config(
             encoder=datetime_encoder_iso,
             decoder=datetime_decoder_iso,
-            mm_field=fields.DateTime(format='iso')
+            mm_field=fields.DateTime(format='iso'),
+            exclude=value_is_none
         )
     else:
         conf = config(
@@ -25,6 +29,7 @@ def datetime_field(json_field_name: Optional[str] = None) -> Field:
             decoder=datetime_decoder_iso,
             mm_field=fields.DateTime(format='iso'),
             field_name=json_field_name,
+            exclude=value_is_none
         )
 
     return field(default=None, metadata=conf)
@@ -32,8 +37,13 @@ def datetime_field(json_field_name: Optional[str] = None) -> Field:
 
 def default(json_field_name: Optional[str] = None) -> Field:
     if json_field_name is None:
-        conf = config()
+        conf = config(
+            exclude=value_is_none
+        )
     else:
-        conf = config(field_name=json_field_name)
+        conf = config(
+            field_name=json_field_name,
+            exclude=value_is_none
+        )
 
     return field(default=None, metadata=conf)
